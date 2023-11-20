6ced575e9fd48e24c4ab0fc1a89bcff3330d8c8a
Damien Beau Wilburn <wilburn.120@osu.edu>
2023-11-20 15:23:42 -0500

    New alignment code based on edlib

--- NanoporeAnalysis/align.py
+++ NanoporeAnalysis/align.py
@@ -1,13 +1,270 @@
 '''
-
 Tools for performing and manipulating sequence alignments
-
 '''
 
 import scipy.stats
 from skbio.alignment import StripedSmithWaterman
 from .utils import reverse_complement
 from cigar import Cigar
+
+import numpy as np
+import edlib
+from NanoporeAnalysis.utils import reverse_complement as rc
+
+def align(query: str,
+          subject: str,
+          max_score: int,
+          score_only: bool = False):
+    '''
+    Align sequences using edlib and compute the edge distance, i.e. steps to make
+    the step sequences match. If score_only, only return the edge distance
+    '''
+    #print( query, subject, max_score, score_only )
+    task = 'distance' if score_only else 'locations'
+    alignment = edlib.align(query, subject, mode='HW', task=task, k=max_score)
+    return alignment
+
+
+def merge_overlapped_indices(index_pairs: list,
+                             tolerated_mismatches: int) -> list:
+    '''
+    Provided a list of (start,end) index pairs, combine overlapping pairs into
+    single pairs that span the full alignment range
+    '''
+    reduced_pairs = []
+    for i, pair in enumerate(index_pairs):
+        if i == 0:
+            current_start, current_end = pair
+        start, end = pair
+        if end <= current_end+1+tolerated_mismatches:
+            current_end = end
+        else:
+            align_len = current_end - current_start + 1
+            reduced_pairs.append({'start':current_start,'end':current_end,'length':align_len})
+            current_start = start
+            current_end = end
+    align_len = current_end - current_start + 1
+    reduced_pairs.append({'start':current_start,'end':current_end,'length':align_len})
+    return reduced_pairs
+ 
+    
+def find_polyX(sequence: str,
+               X: str,
+               N: int,
+               tolerated_mismatches: int,
+               min_len: int) -> list:
+    '''
+    Find runs of single nucleotides (X), using edlib align with search query X*N
+    with tolerated_mismatches in the search
+    '''
+    assert len(X)==1, f'More than one nt provided as quuery ({X}) for find_polyX'
+    query = X*N
+    alignment = align(query, sequence, max_score=0 )
+    align_indices = alignment['locations']
+    if len(align_indices) == 0:
+        return []
+    else:
+        merged_indices = merge_overlapped_indices(align_indices, tolerated_mismatches)
+        filtered_indices = [ i for i in merged_indices if i['length'] >= min_len]
+        sorted_indices = sorted(filtered_indices, key=lambda x:x['length'], reverse=True)
+        return sorted_indices
+    
+    
+def find_best_polyA(sequence: str,
+                    search_N: int=4,
+                    allowed_mismatches: int=1,
+                    min_len: int=10):
+    '''
+    Find best polyA tail
+    CURRENT VERSION: use longest tail, consider adding some 3' filter in
+    '''
+    polyA_tails = find_polyX(sequence, 'A', search_N, allowed_mismatches, min_len )
+    if len(polyA_tails) > 0:
+        return polyA_tails[0]
+    else:
+        return None
+    
+
+
+def split_sequence_by_polyA(sequence: str,
+                            ):
+    '''
+    Find the best polyA tail, and split sequence accordingly
+    Returns the pre-polyA sequence, post-polyA sequence, and the polyA tail itself
+    '''
+    polyA = find_best_polyA(sequence)
+    if polyA:
+        pre_polyA = sequence[:polyA['start']]
+        post_polyA = sequence[polyA['end']+1:]
+    else:
+        pre_polyA = sequence
+        post_polyA = None
+    return pre_polyA, post_polyA, polyA
+        
+    
+    
+def parse_adapter5_seq(sequence:str,
+                       ssp:str,
+                       max_score:int):
+    '''
+    Find 5' SSP and split the read accordingly. If the SSP is not found, then
+    the entire sequence defaults as the transcript sequence.
+    Returns the transcript sequence, adapter sequence, and SSP alignment score
+    '''
+    ssp_alignment = align(ssp,sequence,max_score)
+    if len(ssp_alignment['locations']) > 0:
+        score = (len(ssp)-ssp_alignment['editDistance'])/len(ssp)
+        ssp_locations = ssp_alignment['locations']
+        max_end = np.max([l[1] for l in ssp_locations])
+        adapter_seq = sequence[:max_end+1]
+        transcript_seq = sequence[max_end+1:]
+    else:
+        score = 0.5/len(ssp)
+        adapter_seq = None
+        transcript_seq = sequence
+    return transcript_seq, adapter_seq, score
+    
+        
+        
+        
+def score_umi(putative_umi: str,
+              umi_dict: dict) -> dict:
+    '''
+    Provided a dict with {name:umi}, return the scores per umi sorted by rank
+    '''
+    scores = {}
+    for name, umi in umi_dict.items():
+        umi_align = align(rc(umi), putative_umi, max_score=10, score_only=False)
+        #print(umi_align)
+        if umi_align['editDistance'] == -1:
+            umi_align['editDistance'] = len(umi)
+        umi_score = np.max([len(umi)-umi_align['editDistance'],0.5])/len(umi)
+        scores[name] = umi_score
+    scores = dict(sorted(scores.items(), key=lambda x:x[1], reverse=True))
+    return scores
+
+
+def parse_adapter3_seq(sequence: str,
+                       primer: str,
+                       umi_dict: dict,
+                       max_align_score: int = 10):
+    '''
+    Identify sequence characteristics in the 3' adapter sequence, including any potential UMIs
+    '''
+    primer_alignment = align(primer, sequence, max_score=max_align_score)
+    primer_locations = primer_alignment['locations']
+    #print( sequence, primer, primer_alignment )
+    if len(primer_locations) > 0:
+        primer_score = (len(primer)-primer_alignment['editDistance'])/len(primer)
+        post_start = np.min([l[0] for l in primer_locations])
+    else:
+        primer_score = 0.5/len(primer)
+        post_start = len(sequence) # Retain full sequence for UMI alignment
+    putative_umi = sequence[:post_start]
+    umi_scores = score_umi(putative_umi, umi_dict)
+    return primer_score, umi_scores
+    
+
+def annotate_read(read: dict, 
+                  ssp: str, 
+                  primer3: str, 
+                  umis: dict,
+                  max_polyA_score_len: float = 20.0):
+    '''
+    Search a read for a PolyA tail and then score the 5'/3' ends accordingly
+    
+    *** POSSIBLE PATCH FOR LATER ***
+    Currently max_polyA_score_len is arbitrary, consider changing later
+    ***
+    
+    '''
+    sequence = read['sequence']
+    pre_polyA, post_polyA, polyA = split_sequence_by_polyA(sequence)
+    if post_polyA:
+        # Score potential UMIs in 3' adapter sequence
+        primer_3_score, umi_scores = parse_adapter3_seq(post_polyA, primer3, umis)
+        polyA_score = np.min([polyA['length']/max_polyA_score_len, 1.0])
+    else:
+        primer_3_score = 0.5/len(primer3)
+        umi_scores = dict([(umi,0.5/len(umi)) for umi in umis])
+        polyA_score = 0.5/max_polyA_score_len
+    transcript_seq, adapter5_seq, primer_5_score = parse_adapter5_seq(pre_polyA, ssp, 10) # HARD CODED MAX SCORE ATM, CHANGE LATER
+    read_annotations = { '3\' primer':                 primer3,
+                         '3\' primer alignment score': primer_3_score,
+                         '3\' adapter sequence':       post_polyA,
+                         'Strand switch primer':       ssp,
+                         'SSP alignment score':        primer_5_score,
+                         '5\' adapter sequence':       adapter5_seq,
+                         'UMI Scores':                 umi_scores,
+                         'PolyA coordinates':          polyA,
+                         'PolyA score':                polyA_score,
+                         'Transcript sequence':        transcript_seq,
+                       }
+    return read | read_annotations
+    
+    
+def process_read(read: dict,
+                 ssp: str,
+                 primer3: str,
+                 umis: dict,
+                 min_polyA_score: float = 0.5,
+                 min_ssp_score: float = 0.5):
+    '''
+    Annotate reads and, if possible, re-orient along forward strand
+    
+    *** POSSIBLE PATCH FOR LATER ***
+    Currently min_polyA_score and min_ssp_score are *very* arbitrarily chosen
+    based on examination of real data; usually real scores are substantially
+    better than 0.5, but seemed like a good enough starting point. Consider
+    changing based on further testing.
+    ***
+    '''
+    forward_analysis = annotate_read(read, ssp, primer3, umis)
+    rc_read = dict(read)
+    rc_read['sequence'] = rc(rc_read['sequence'])
+    reverse_analysis = annotate_read(rc_read, ssp, primer3, umis)
+    
+    if np.max([forward_analysis['PolyA score'],reverse_analysis['PolyA score']]) > min_polyA_score:
+        # Found a PolyA tail
+        delta_polyA_score = forward_analysis['PolyA score'] - reverse_analysis['PolyA score']
+        if np.abs(delta_polyA_score) > min_polyA_score:
+            analysis = forward_analysis if delta_polyA_score > 0 else reverse_analysis
+            annotation = 'Full-length cDNA' if analysis['SSP alignment score'] > min_ssp_score else "3\' only"
+        else:
+            analysis = forward_analysis # Need to actually solve for the correct one
+            annotation = "Duplexed 3\' only"
+        best_umi, second_umi = list(analysis['UMI Scores'])[:2]
+        best_umi_score = analysis['UMI Scores'][best_umi]
+        delta_umi_score = best_umi_score - analysis['UMI Scores'][second_umi]
+        
+    else:
+        # Did not find a PolyA tail, check for 5' features
+        best_umi, best_umi_score, delta_umi_score = [None]*3
+        max_ssp_score = np.max([forward_analysis['SSP alignment score'],reverse_analysis['SSP alignment score']])
+        if max_ssp_score > min_ssp_score:
+            delta_ssp_score = forward_analysis['SSP alignment score']-reverse_analysis['SSP alignment score']
+            if np.abs(delta_ssp_score) > min_ssp_score:
+                analysis = forward_analysis if delta_ssp_score > 0 else reverse_analysis
+                annotation = "5\' only"
+            else:
+                analysis = forward_analysis # Need to solve for which one is better
+                annotation = "Duplexed 5\' only"
+        else:
+            analysis = forward_analysis # Unknown, no better guess
+            annotation = "Uncharacterized sequence"
+            
+    analysis['annotation'] = annotation 
+    analysis['Best UMI'] = best_umi
+    analysis['Best UMI Score'] = best_umi_score
+    analysis['Delta UMI Score'] = delta_umi_score
+    return analysis, forward_analysis, reverse_analysis
+
+
+
+
+
+
+
 
 def align_to_dict(align_result):
     """Convert a skbio.alignment object into a simple dict.
